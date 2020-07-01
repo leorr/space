b6bf167310096026131585da8dd7bf1ce1bb3319
leorr <leonardoribeiro.2016@alunos.utfpr.edu.br>
2020-07-01 17:46:43 -0300

    2020/07/01 17:46

--- Scripts/RigidPlayer.gd
+++ Scripts/RigidPlayer.gd
@@ -1,7 +1,6 @@
 extends RigidBody2D
 
 onready var planets = get_tree().get_nodes_in_group("Planet")
-onready var gravity = (get_position() - planets[0].get_position()).normalized()
 onready var animator = get_node("anim_player")
 
 enum{
@@ -14,23 +13,28 @@ onready var _transitions: = {
 	RUN:[IDLE],
 }
 
-export var run_speed := 400.0
+export var run_speed := 4000.0
 export var air_speed := 10.0
 export var jump_force := 500.0
 
 var _state: int = IDLE
 var _pstate: int = 99
+var move_direction = Vector2()
+var flip : bool = false
 
 var states_strings := {
 	IDLE: "idle",
 	RUN: "run",
 }
 
+func _process(_delta):
+	move_direction = get_move_direction()
+	set_flip()
+	
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	#var grounded := state.get_contact_count() > 0
-	var move_direction := get_move_direction()
-	
-	print("pstate:", _pstate , " state:", _state, "\n")
+	#print("pstate:", _pstate , " state:", _state, "\n")
+	rotate((get_position() - planets[0].get_position()).normalized().angle_to(Vector2(0,-1))*-1)
 	match _state:
 		IDLE:
 			if move_direction:
@@ -38,8 +42,8 @@ func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 		RUN:
 			if not move_direction:
 				change_state(IDLE)
-			else:
-				state.linear_velocity = move_direction * run_speed
+			elif linear_velocity.length() < 100:
+				state.apply_central_impulse(move_direction*run_speed*0.5)
 		
 func change_state(target_state: int) -> void:
 	if (not target_state in _transitions[_state]) or _pstate == _state:
@@ -54,7 +58,15 @@ func enter_state() -> void:
 			animator.play("Idle")
 		RUN:
 			animator.play("Walking")
-func get_move_direction() -> Vector2:
-	return (get_position() - planets[0].get_position()).normalized().tangent() * (-Input.get_action_strength("ui_right") +Input.get_action_strength("ui_left"))
-	#here i need to get the vector tangent to the gravity.
 
+func get_move_direction() -> Vector2:
+	return (get_position() - planets[0].get_position()).normalized().tangent() * (
+			-Input.get_action_strength("ui_right") + Input.get_action_strength("ui_left"))
+
+func set_flip() -> void:
+	if Input.is_action_pressed("ui_left"):
+		flip=1
+	elif Input.is_action_pressed("ui_right"): 
+		flip=0
+	animator.set_flip_h(flip)
+	return
